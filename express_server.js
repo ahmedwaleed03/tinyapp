2b3e61f58414019521ecc1356b8d01a9187c3872
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-09-06 19:40:54 -0400

    added user registration

--- express_server.js
+++ express_server.js
@@ -23,6 +23,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
@@ -30,14 +43,27 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]], };
   res.render("urls_new", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  const {email, password} = req.body;
+  let newId = generateRandomString();
+  let newUser = {
+    id: newId,
+    email,
+    password
+  };
+  users[newId] = newUser;
+  res.cookie("user_id", newId);
+  res.redirect("/");
 });
 
 app.post("/urls", (req, res) => {
@@ -53,6 +79,7 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+
 app.post("/urls/:id/delete", (req, res) => {
   const { id } = req.params;
   delete urlDatabase[id];
@@ -63,13 +90,13 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
     longURL: urlDatabase[req.params.id], 
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_show", templateVars);
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { username: req.cookies["username"]};
+  const templateVars = { user: users[req.cookies["user_id"]],};
   res.render("registration", templateVars);
 });
 
@@ -85,7 +112,7 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect("/");
 });
 
